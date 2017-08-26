6dc596ac2b0e223eb6374641592b131ccdbffcec
meerimkanatbek <meerimkanatbek@192.168.1.44>
2017-08-26 16:15:15 -0400

    Clean Cukes

--- src/test/java/com/fourstay/runners/CukesRunner.java
+++ src/test/java/com/fourstay/runners/CukesRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-<<<<<<< HEAD
+
 
 @CucumberOptions(
 		plugin = "html:target/cucumber-reports",
@@ -17,16 +17,6 @@ import cucumber.api.junit.Cucumber;
 		
 		)
 
-=======
-@CucumberOptions(
-		plugin = "html:target/cucumber-reports",
-		features = "src/test/resources/features", 
-		glue = "com/fourstay/step_definitions", 
-		tags = "@Staytest_125",
-		dryRun = true
-		
-		)
->>>>>>> branch 'master' of https://github.com/cbttestuser1/automation-project-test.git
 public class CukesRunner {
 
 }
