8f15a72faf978557626c4a96878247e694c1d050
artiom1997 <elena.kostiouk@gmail.com>
2017-08-26 16:17:17 -0400

    new comment

--- src/test/java/com/fourstay/runners/CukesRunner.java
+++ src/test/java/com/fourstay/runners/CukesRunner.java
@@ -7,9 +7,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "html:target/cucumber-reports", features = "src/test/resources/features", glue = "com/fourstay/step_definitions", tags = "@Staytest_125", dryRun = true
-// adding a useless comment
 
-// hello!!
+// please dont ruine this!
 
 )
 public class CukesRunner {
