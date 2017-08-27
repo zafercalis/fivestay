49d248ec4e79bf63c6574d3dc91a962c5590d691
shahrihan <behruzahon7@gmail.com>
2017-08-27 09:56:21 -0400

    Cleaned version!

--- src/test/java/com/fourstay/runners/CukesRunner.java
+++ src/test/java/com/fourstay/runners/CukesRunner.java
@@ -7,10 +7,11 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "html:target/cucumber-report",
-		"json:target/cucumber.json" }, features = "src/test/resources/features", glue = "com/fourstay/step_definitions",
-		// tags="@Staytest_125, @Staytest_124, @search, @homepage, @login",
-		tags = "@Staytest_125", dryRun = true)
-// just adding a comment
+		"json:target/cucumber.json" }, 
+features = "src/test/resources/features", glue = "com/fourstay/step_definitions",
+		
+		tags = "@Staytest_125", dryRun = false)
+
 public class CukesRunner {
 
 }
